880e89f99f611b9f36fb99ec39ded8803a567aed
oriexsol <62597130+oriexsol@users.noreply.github.com>
2020-10-09 15:29:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ apt-get install -y docker-compose
 mkdir /nitur
 cp influxdb.conf /nitur/
 cp telegraf.conf /nitur/
+cd ..
 docker load -i telegraf.tar
 docker load -i grafana.tar
 docker load -i influxdb.tar
